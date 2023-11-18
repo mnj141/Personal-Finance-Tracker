d4d40a4ab5de06f1cafb6620430ad7d4c6e8594e
chidipothupraveenkumar <123039402+chidipothupraveenkumar@users.noreply.github.com>
2023-11-17 21:29:12 -0600

    Updated Register activity with Db details and xmls

--- app/src/main/java/com/example/personalfinancetracker01/auth/RegisterActivity.java
+++ app/src/main/java/com/example/personalfinancetracker01/auth/RegisterActivity.java
@@ -1,26 +1,39 @@
 package com.example.personalfinancetracker01.auth;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
-import com.example.personalfinancetracker01.MainActivity;
 import com.example.personalfinancetracker01.R;
+import com.example.personalfinancetracker01.data.AppDao;
+import com.example.personalfinancetracker01.data.AppDatabase;
+import com.example.personalfinancetracker01.data.User;
+import com.example.personalfinancetracker01.helper.Utils;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
-    private EditText etEmail, etPassword, etUserName, etMobileNumber, etRepeatPassword;
+    private EditText etEmail, etPassword, etFirstname, etLastName, etMobileNumber, etRepeatPassword;
 
+    AppDatabase db;
+    AppDao dao;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        db = AppDatabase.getDatabase(getApplicationContext());
+        dao = db.appDao();
 
         // Hide the action bar (title bar)
         if (getSupportActionBar() != null) {
@@ -35,9 +48,10 @@ public class RegisterActivity extends AppCompatActivity {
 
 
         etEmail = findViewById(R.id.etEmail);
+        etFirstname = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
         etPassword = findViewById(R.id.etPassword);
         etRepeatPassword = findViewById(R.id.etRepeatPassword);
-        etUserName = findViewById(R.id.etUserName);
         etMobileNumber = findViewById(R.id.etMobile);
     }
 
@@ -57,12 +71,68 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void onClickOnSignup(View view) {
-        createUser(etEmail.getText().toString(), etPassword.getText().toString());
-    }
 
-    private void createUser(String emailId, String password) {
-        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
-        finish();
+        if (etFirstname.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter First Name", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (etLastName.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter Last Name", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (!Utils.isValidEmail(etEmail.getText().toString())) {
+            Toast.makeText(this, "Enter Valid Email Address", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (etPassword.getText().length() <= 3 || etPassword.getText().length() >= 16) {
+            Toast.makeText(this, "Password length should be minimum 4 and maximum 16 characters long", Toast.LENGTH_LONG).show();
+            return;
+        }
+//        if (!Utils.isValidPassword(etPassword.getText().toString())) {
+//            Toast.makeText(this, "Password should contains at least one uppercase, lowercase, special character and number", Toast.LENGTH_LONG).show();
+//            return;
+//        }
+        if (!etPassword.getText().toString().equals(etRepeatPassword.getText().toString())) {
+            Toast.makeText(this, "Password and Confirm password should match", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (etMobileNumber.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Please enter mobile number", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+       /* DatabaseManager.getInstance().setEmail(etEmail.getText().toString());
+        DatabaseManager.getInstance().setMobile(etMobileNumber.getText().toString());
+         DatabaseManager.getInstance().setFirstName(etFirstname.getText().toString());
+        DatabaseManager.getInstance().setLastName(etLastName.getText().toString());
+        createUser(etEmail.getText().toString(), etPassword.getText().toString());*/
+
+        dao.checkEmailExists(etEmail.getText().toString()).observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                if (integer > 0) {
+                    Toast.makeText(RegisterActivity.this, "Email already exist", Toast.LENGTH_LONG).show();
+                } else {
+                    AppDatabase.databaseWriteExecutor.execute(() -> {
+                        dao.registerUser(new User(
+                                etEmail.getText().toString(),
+                                etFirstname.getText().toString(),
+                                etLastName.getText().toString(),
+                                etPassword.getText().toString(),
+                                etMobileNumber.getText().toString()
+                        ));
+                    });
+                    Toast.makeText(RegisterActivity.this, "Registered Successfully!", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    finishAffinity();
+                }
+            }
+        });
+
 
     }
 }
