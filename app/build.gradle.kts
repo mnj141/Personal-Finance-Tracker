4362e68a448e0200eb5eb60983dcece77462cb16
mnj141 <123129651+mnj141@users.noreply.github.com>
2023-11-17 20:56:38 -0600

    updated build.gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     id("com.android.application")
 }
 
+
 android {
-    namespace = "com.example.personalfinancetracker01"
-    compileSdk = 33
+    namespace = "edu.nwmissouri.personalfinancetracker"
+    compileSdk = 32
 
     defaultConfig {
-        applicationId = "com.example.personalfinancetracker01"
-        minSdk = 24
-        targetSdk = 33
+        applicationId = "edu.nwmissouri.personalfinancetracker"
+        minSdk = 25
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
@@ -33,10 +34,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.7.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+
+    implementation("androidx.room:room-runtime:2.4.0")
+    annotationProcessor("androidx.room:room-compiler:2.4.0")
+
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 }
